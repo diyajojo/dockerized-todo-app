165d32ebee4ca6c39d18d67c3a4bcb4e93c1ace8
diyajojo <diyaj2122@gmail.com>
2025-07-26 20:09:20 +0530

    proxy error

--- todo-app/client/vite.config.js
+++ todo-app/client/vite.config.js
@@ -10,9 +10,10 @@ export default defineConfig({
     },
   },
   server: {
+    host:true,
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://backend:3000',
     },
   },
   build: {
